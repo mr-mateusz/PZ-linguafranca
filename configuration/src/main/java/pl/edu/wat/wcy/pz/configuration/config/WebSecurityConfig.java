f6be080f2e145fb0a24315556e9f21d792947a3d
mr-mateusz <37747987+mr-mateusz@users.noreply.github.com>
2019-04-01 08:54:51 +0200

    created configuration and discovery modules

---
+++ configuration/src/main/java/pl/edu/wat/wcy/pz/configuration/config/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+package pl.edu.wat.wcy.pz.configuration.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/actuator/**").permitAll()
+                .anyRequest().authenticated()
+                .and().httpBasic();
+
+    }
+}
